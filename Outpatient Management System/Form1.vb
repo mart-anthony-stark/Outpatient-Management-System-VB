520c8f3827bd31b0ef1fa3487b0f56bac02ce025
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-14 20:11:41 +0800

    initial commit

---
+++ Outpatient Management System/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Login
+
+    Private Sub Login_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.CenterToScreen()
+    End Sub
+    
+End Class
