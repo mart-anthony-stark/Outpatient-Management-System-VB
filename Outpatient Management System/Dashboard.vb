4b52918f801afc61e58cde4e507a877f32674cdb
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-16 20:21:28 +0800

    add dashboard form

---
+++ Outpatient Management System/Dashboard.vb
@@ -0,0 +1,6 @@
+﻿Public Class dashboard
+
+    Private Sub Form2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.CenterToScreen()
+    End Sub
+End Class
