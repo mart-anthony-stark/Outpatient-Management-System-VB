18d26d3097e977c744f076724dbfc5a5ab3d9a15
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-18 20:29:01 +0800

    add tabs

--- Outpatient Management System/Dashboard.vb
+++ Outpatient Management System/Dashboard.vb
@@ -3,4 +3,16 @@
     Private Sub Form2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         Me.CenterToScreen()
     End Sub
+
+    Private Sub TabPage1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TabPage1.Click
+
+    End Sub
+
+    Private Sub TabPage2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TabPage2.Click
+
+    End Sub
+
+    Private Sub AsdToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
 End Class
