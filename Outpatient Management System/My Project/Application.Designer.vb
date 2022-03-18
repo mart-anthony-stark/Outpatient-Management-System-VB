185ab3926345a4150b4417469da9e5b5dca8e3db
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-18 20:56:56 +0800

    add note app

--- Outpatient Management System/My Project/Application.Designer.vb
+++ Outpatient Management System/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.WindowsApplication1.dashboard
+            Me.MainForm = Global.WindowsApplication1.Notes
         End Sub
     End Class
 End Namespace
