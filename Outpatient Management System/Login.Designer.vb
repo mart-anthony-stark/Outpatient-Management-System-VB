129568d573c4744a48736f5b0387f97cf3ed5db0
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-19 22:21:29 +0800

    resize login form

--- Outpatient Management System/Login.Designer.vb
+++ Outpatient Management System/Login.Designer.vb
@@ -34,7 +34,7 @@ Partial Class Login
         '
         Me.loginBtn.BackColor = System.Drawing.Color.MidnightBlue
         Me.loginBtn.ForeColor = System.Drawing.Color.White
-        Me.loginBtn.Location = New System.Drawing.Point(319, 310)
+        Me.loginBtn.Location = New System.Drawing.Point(377, 311)
         Me.loginBtn.Name = "loginBtn"
         Me.loginBtn.Size = New System.Drawing.Size(260, 42)
         Me.loginBtn.TabIndex = 0
@@ -46,19 +46,19 @@ Partial Class Login
         Me.Panel1.BackColor = System.Drawing.Color.MidnightBlue
         Me.Panel1.Location = New System.Drawing.Point(0, -1)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(256, 429)
+        Me.Panel1.Size = New System.Drawing.Size(256, 517)
         Me.Panel1.TabIndex = 1
         '
         'username
         '
-        Me.username.Location = New System.Drawing.Point(319, 144)
+        Me.username.Location = New System.Drawing.Point(377, 145)
         Me.username.Name = "username"
         Me.username.Size = New System.Drawing.Size(260, 20)
         Me.username.TabIndex = 2
         '
         'password
         '
-        Me.password.Location = New System.Drawing.Point(319, 211)
+        Me.password.Location = New System.Drawing.Point(377, 212)
         Me.password.Name = "password"
         Me.password.Size = New System.Drawing.Size(260, 20)
         Me.password.TabIndex = 3
@@ -68,7 +68,7 @@ Partial Class Login
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.MidnightBlue
-        Me.Label1.Location = New System.Drawing.Point(315, 117)
+        Me.Label1.Location = New System.Drawing.Point(373, 118)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(105, 24)
         Me.Label1.TabIndex = 4
@@ -79,7 +79,7 @@ Partial Class Login
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label2.ForeColor = System.Drawing.Color.MidnightBlue
-        Me.Label2.Location = New System.Drawing.Point(315, 184)
+        Me.Label2.Location = New System.Drawing.Point(373, 185)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(100, 24)
         Me.Label2.TabIndex = 5
@@ -89,7 +89,7 @@ Partial Class Login
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(633, 427)
+        Me.ClientSize = New System.Drawing.Size(806, 518)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.password)
