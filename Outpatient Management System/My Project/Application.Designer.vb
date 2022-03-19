19dd4cc7f30969ec996337f8084a89d10ff8fb0f
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-19 22:19:39 +0800

    add statistics form

--- Outpatient Management System/My Project/Application.Designer.vb
+++ Outpatient Management System/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.WindowsApplication1.Notes
+            Me.MainForm = Global.WindowsApplication1.Login
         End Sub
     End Class
 End Namespace
--- Outpatient Management System/Statistics.Designer.vb
+++ Outpatient Management System/Statistics.Designer.vb
@@ -22,8 +22,16 @@ Partial Class Statistics
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.SuspendLayout()
+        '
+        'Statistics
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(915, 533)
+        Me.Name = "Statistics"
         Me.Text = "Statistics"
+        Me.ResumeLayout(False)
+
     End Sub
 End Class
