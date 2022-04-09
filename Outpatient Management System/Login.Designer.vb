b830b9ae22ecc826c26ca837b5e7ed554360a174
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-09 23:32:13 +0800

    Add msql connection connector v6.1.2

--- Outpatient Management System/Login.Designer.vb
+++ Outpatient Management System/Login.Designer.vb
@@ -28,6 +28,7 @@ Partial Class Login
         Me.password = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'loginBtn
@@ -85,11 +86,21 @@ Partial Class Login
         Me.Label2.TabIndex = 5
         Me.Label2.Text = "Password"
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(386, 403)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(250, 39)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "CheckConnection"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'Login
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(806, 518)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.password)
@@ -108,5 +119,6 @@ Partial Class Login
     Friend WithEvents password As System.Windows.Forms.TextBox
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
 
 End Class
