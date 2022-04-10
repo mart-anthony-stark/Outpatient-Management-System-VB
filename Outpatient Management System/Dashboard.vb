097bf9a5166241deb6e7e69672e356516a1627de
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-11 00:04:53 +0800

    Add retrieve data

--- Outpatient Management System/Dashboard.vb
+++ Outpatient Management System/Dashboard.vb
@@ -2,6 +2,11 @@
 
     Private Sub Form2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         Me.CenterToScreen()
+        Try
+            reload("SELECT * FROM employee", DataGridView1)
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
     End Sub
 
     Private Sub TabPage1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TabPage1.Click
@@ -13,6 +18,14 @@
     End Sub
 
     Private Sub AsdToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+
+    Private Sub AasdqweToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
 
     End Sub
 End Class
